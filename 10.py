c71e1ee819cfce16fa4f40633ab19382069980a8
whoami5502 <139572195+whoami5502@users.noreply.github.com>
2023-07-19 21:53:30 -0300

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,5 @@
+real = float(input('Digite quanto dinheiro você tem: R$'))
+dolar = real / 4.81
+iene = real / 0.035
+
+print(f'Com R${real:.2f} vocẽ pode comprar US${dolar:.2f} e {iene:.2f} ienes.')
