1d06764132220a1f2d1631d363525caa603c60fa
whoami5502 <139572195+whoami5502@users.noreply.github.com>
2023-07-19 21:52:04 -0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,9 @@
+n = input('Digite seu nome:')
+print(f'Olá {n}, quero que voce digite 1 número e eu irei dizer o antecessor e o sucessor dele. ')
+
+n1 = int(input('Digite o número:'))
+s = n1 + 1
+a = n1 - 1
+
+print(f'O antecessor do número {n1} é {a} e o sucessor é {s}')
+print(f'Tchau {n}!')
