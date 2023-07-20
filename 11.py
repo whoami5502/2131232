7f362621e9d3a859d9e2c6761d765d40d037d4ba
whoami5502 <139572195+whoami5502@users.noreply.github.com>
2023-07-19 21:53:44 -0300

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,7 @@
+larg = float(input('Digite a largura da parede: '))
+alt = float(input('Digite a altura da parede: '))
+a = larg * alt
+t = a / 2
+
+print(f'A sua parede tem a dimensão de {larg} x {alt} e sua área é igual a {a}m²')
+print(f'Para pintar a sua parede de {a}m² é necessario ter {t}l de tinta')
